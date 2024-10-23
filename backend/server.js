00058214ce31d3b6f222eeb8871d6562f5ed4bcb
annahico <annahico@gmail.com>
2024-10-23 20:18:42 +0200

    update: updated server.js with routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const colaboradorRoutes = require('./routes/colaboradorRoutes');
 require('dotenv').config();
+
+app.use('/api/colaboradores', colaboradorRoutes);
 
 const app = express();
 app.use(cors());
